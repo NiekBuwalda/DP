3235f69566fe5b0384dedb34168ff6e9190893c8
Niek <heraniek@hotmail.com>
2018-11-29 12:06:13 +0100

    removed binaries and makefiles

--- sylvan/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /vol/home/s1560700/Documents/Master/jaar2/final_vec/piSat/sylvan
-# Build directory: /vol/home/s1560700/Documents/Master/jaar2/final_vec/piSat/sylvan
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(src)
-subdirs(test)
--- sylvan/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /vol/home/s1560700/Documents/Master/jaar2/final_vec/piSat/sylvan/src
-# Build directory: /vol/home/s1560700/Documents/Master/jaar2/final_vec/piSat/sylvan/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
